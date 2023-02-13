0b4ff0fd1cdf11e04d817f3ec2e95161e4c2cb08
Ivana Belak <ivanabelak996@gmail.com>
2023-02-13 18:52:59 +0100

    show date in HTML

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("Hello world");
 console.log("Hello world2");
+
+dateElement = document.querySelector("#date");
+newDate = new Date();
+
+dateElement.innerHTML = newDate.toLocaleString();
